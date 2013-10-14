d41890d0bd799585cf7ee2b881b7d2a0488a675b
Ketaki Joshi <jkets123@gmail.com>
2013-10-13 20:12:59 -0500

    Second draft of Search Tool,Searches for every element of list provided

--- Search_Tool.py
+++ Search_Tool.py
@@ -8,12 +8,15 @@ import selenium.webdriver.support.ui as ui
 def search(parameter):
 	browser=webdriver.Firefox()
 	browser.get("http://www.google.com/")
-	text_box=browser.find_element_by_id('gbqfq')
-	text_box.send_keys(parameter)
-	button=browser.find_element_by_id('gbqfb');
-	button.click()
-	#browser.close()
-
+	for p in range(0,6):
+		#text_box=browser.find_element_by_id('gbqfq')
+		#text_box.send_keys("cats")
+		#button=browser.find_element_by_id('gbqfb');
+		#button.click()
+		browser.get("https://www.google.com/#q="+parameter[p])
+		time.sleep(6)
+	browser.close()
 
 if __name__=="__main__":
-	search("dogs")
+	parameter=['dogs','cats','cars','trains','ships','Chicago']
+	search(parameter)
